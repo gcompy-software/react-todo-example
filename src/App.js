052de54d67287060f985921f3a43c219c16b7fbd
gcompy <gcompy@Mac-Guido.local>
2020-07-20 22:06:56 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
+import TodoApp from './TodoApp';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+     super();
+     this.state = {
+       data: [1,2,3,4,5]
+     }
+     this.delete = this.delete.bind(this);
+  }
+  
+  delete(id){
+     this.setState(prevState => ({
+         data: prevState.data.filter(el => el !== id )
+     }));
+  }
+  
+  render(){
+     return(
+         <Child delete={this.delete} data={this.state.data}/>
+     );
+  }
 }
+
+class Child extends React.Component{
+
+  delete(id){
+      this.props.delete(id);
+  }
+  
+  render(){
+     return(
+        <div>
+          {
+             this.props.data.map(el=>
+                 <p onClick={this.delete.bind(this, el)}>{el}</p>
+             )
+          }
+          <TodoApp />
+        </div>
+     )
+  }
+}
+
+
+ReactDOM.render(<App/>, document.getElementById('app'))
+
+
 
 export default App;
