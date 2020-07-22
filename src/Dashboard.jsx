9c5975f0f5f7ab0477af8e95fc60f3730e77468b
gcompy <gcompysoftware@gmail.com>
2020-07-21 23:14:51 -0400

    CRUD basic

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,103 +1,107 @@
 import React from 'react'
 
 class Dashboard extends React.Component {
-   
-    delete(id){
-        this.props.delete(id);
-     }
 
-       render() {
-           return (
-               <div>
-                   {
-                      this.props.items.map((item)=>
-                      <p key={item.id}>
-                         {item.id}
-                         {item.title}
-                         {item.person}
-                         {item.state}
-                         {item.deadline}
-                         {item.created}
-                         <button onClick={this.delete.bind(this, item)}>Delete</button>
-                      </p>
-                      )
-                   }
-               </div>
-           );
-       }
-   }
+constructor(props) {
+    super(props)
+    this.state = {
+        items: [],
+        id: '', 
+        title: '',
+        person: '',
+        state: '',
+        deadline: '',
+        created: '',        
+        }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+}
 
-class Task extends React.Component {
-      constructor(props) {
-          super(props);
-          this.state = {
-             items: [],
-             id: '', 
-             title: '',
-             person: '',
-             state: '',
-             deadline: '',
-             created: '',
-            }
-            this.handleChange = this.handleChange.bind(this)
-            this.handleSubmit = this.handleSubmit.bind(this)
-            this.delete = this.delete.bind(this)
-      }
-  
-      render() {
-          return (
-              <div>
-                  <h3>TODO</h3>
-                  <Dashboard items={this.state.items} delete={this.delete}/>
-                  <form onSubmit={this.handleSubmit}>
-                      <input id="title"onChange={this.handleChange} value={this.state.title} />
-                      <input id="person" onChange={this.handleChange} value={this.state.person} />
-                      <input id="state" onChange={this.handleChange} value={this.state.state} />
-                      <input id="deadline" onChange={this.handleChange} value={this.state.deadline} />
-                      <input id="created" onChange={this.handleChange} value={this.state.created} />
-                      <button>{'Add #' + (this.state.items.length + 1)}</button>
-                  </form>
-              </div>
-          );
-      }
-      
-      handleChange(e) {
-         console.log(e.target.id)
-          this.setState({
-            [e.target.id]: e.target.value
-         });
-      }
+handleChange(e) {
+    console.log(e.target.id)
+     this.setState({
+       [e.target.id]: e.target.value
+    });
+ }
 
-      handleSubmit(e) {
-          e.preventDefault();
-          var newItem = {
-              id: Date.now(),
-              title: this.state.title,
-              person: this.state.person,
-              state: this.state.state,
-              deatline: this.state.deatline,
-              created: this.state.created,
-              
-          };
-          this.setState((prevState) => ({
-              items: prevState.items.concat(newItem),
-              id: '', 
-              title: '',
-              person: '',
-              state: '',
-              deadline: '',
-              created: ''
-          }));
-      }
-  
-      delete(id){          
-          this.setState(prevState => ({
-            items: prevState.items.filter(el => el !== id )
-        }));
-      }
-  }
-  
+ clear(){
+    this.setState((prevState) => ({
+        items : prevState.items,
+        id: '', 
+        title: '',
+        person: '',
+        state: '',
+        deadline: '',
+        created: '',
+    }))
+ }
 
-  
+ delete(id){          
+    this.setState(prevState => ({
+        items: prevState.items.filter(el => el !== id )
+    }))
+    this.clear()
+ }
+
+ edit(item){  
+    this.setState((prevState) => ({
+        items:prevState.items,...item
+    }))
+ }
+
+handleSubmit(e) { 
+    e.preventDefault();
+
+    var data = {
+        id: this.state.id || Date.now(),
+        title: this.state.title,
+        person: this.state.person,
+        state: this.state.state,
+        deadline: this.state.deadline,
+        created: Date.now(),
+    }
+
+    if(!this.state.id){ //Add row   
+        this.setState(prevState=>({
+            items: [...prevState.items,data]
+        }))
+    }else{ //update row
+        this.setState(
+            {
+                items: [data]
+            })     
+    }
+    this.clear()
+}
+
+    render() {
+        return (
+            <div>
+                {
+                    this.state.items.map((item)=>
+                    <p key={item.id}>
+                        {item.id}
+                        {item.title}
+                        {item.person}
+                        {item.state}
+                        {item.deadline}
+                        {item.created}
+                        <button onClick={this.delete.bind(this, item)}>Delete</button>
+                        <button onClick={this.edit.bind(this, item)}>Edit</button>
+                    </p>
+                    )
+                }
+                   <form onSubmit={this.handleSubmit}>
+                       <input id="title" onChange={this.handleChange} value={this.state.title} />
+                       <input id="person" onChange={this.handleChange} value={this.state.person} />
+                       <input id="state" onChange={this.handleChange} value={this.state.state} />
+                       <input id="deadline" onChange={this.handleChange} value={this.state.deadline} />
+                       <input id="created" onChange={this.handleChange} value={this.state.created} />
+                       <button type="submit">Add</button>
+                   </form>
+            </div>
+        );
+    }
+}
 
 export default Dashboard
